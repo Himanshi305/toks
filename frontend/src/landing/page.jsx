cab6b5aacef67492bf871ac1d8c39169c96f33a5
Himanshi <himanshigupta10425@gmail.com>
2026-01-21 00:08:35 +0530

    chore: initialized repo

--- frontend/src/landing/page.jsx
+++ frontend/src/landing/page.jsx
@@ -5,6 +5,8 @@ const landing = () => {
   return (
     <div className="fade-in">
       <Navbar />
+      <div className="border border-white rounded-full h-5 w-5"></div>
+      <div className="relative top-50 justify-center items-center flex font-mono text-5xl">Welcome to <div className="inline-block mx-6 font-bold text-6xl text-purple-500">Toks</div></div>
     </div>
   )
 }
