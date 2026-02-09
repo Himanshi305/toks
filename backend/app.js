8e043d1646dd34755d888be79071282c086c0e59
Himanshi <himanshigupta10425@gmail.com>
2026-02-09 15:25:23 +0530

    chore: initialized repo

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use('/api/user', userRoutes);
 app.use('/api/projects', projectRoutes);
