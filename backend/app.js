2628ca7d456dc61c8751f2f1de5ae2e9fcf8ff09
Himanshi <himanshigupta10425@gmail.com>
2026-02-10 16:28:18 +0530

    chore: initialized repo

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,6 @@ const corsOptions = {
 
 app.use(morgan('dev'));
 app.use(cors(corsOptions));
-app.options("/*", cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
